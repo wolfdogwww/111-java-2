374e4317e91b9600405afc3656db3505e20bce2c
worlfdog <73741997+wolfdogwww@users.noreply.github.com>
2023-03-10 11:24:43 +0800

    Delete poker.java

--- 2023-3-3/poker.java
+++
@@ -1,5 +0,0 @@
-class Poker{
-    public String suits[]={"spade","heart","diamond","club"};
-    public String num[]={"one","two","three","four","five","six","seven","eight","night","ten","jack","queen","king"};
-    
-}
